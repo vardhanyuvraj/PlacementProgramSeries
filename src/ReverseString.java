1395c944722f2ff07f79428c462abb2d0582b647
vardhanyuvraj <vardhan.yuvraj.singh@gmail.com>
2022-11-02 00:34:05 +0530

    added comments to swap reverse string

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -6,21 +6,39 @@ public class ReverseString {
 		System.out.println(reverseString(str));
 	}
 
+	// function to reverse the string
 	public static String reverseString(String str) {
+
+		// if string is single character then return the same string
 		if (str.length() < 2) {
 			return str;
 		} else {
+
+			// created character array by converting current string to character array
 			char[] stringToArray = str.toCharArray();
+
+			/*
+			 * initialized i to starting index of string and j to last index of string
+			 * incrementing the value of i and decrementing the value of j till i remains
+			 * less than j
+			 * 
+			 */
 			for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+
+				// swapping the character at front index with character at the last index
 				char temp = stringToArray[i];
 				stringToArray[i] = stringToArray[j];
 				stringToArray[j] = temp;
 			}
+
+			// printing the value of character array using for each loop
 			for (char ch : stringToArray) {
 				System.out.print(ch);
 			}
 		}
-		return "";
+
+		// return null if string passed is null
+		return null;
 	}
 
 }
