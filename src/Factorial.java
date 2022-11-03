862351223242b21ff4a999b4e1d0a8c540fbb021
Yuvraj Vardhan <vardhan.yuvraj.singh@gmail.com>
2022-11-03 16:55:14 +0530

    added documentation to factorial

--- src/Factorial.java
+++ src/Factorial.java
@@ -6,7 +6,12 @@ public class Factorial {
 	}
 
 	public static int factorial(int n) {
+
+		// intitialized a varaible to 1
 		int fact = 1;
+
+		// loop that will run from 1 till the number and will keep multiplying the value
+		// of i with the value of fact
 		for (int i = 1; i <= n; i++) {
 			fact = fact * i;
 		}
